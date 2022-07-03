8ef27bcfc60b553b6ecbe57a5f2a54bca5a2ffdb
brokenfingers <buvotrys@gmail.com>
2022-07-03 11:23:17 +0300

    init

---
+++ src/components/Users/AddUser.tsx
@@ -0,0 +1,16 @@
+const AddUser = () => {
+
+    const addUserHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
+
+    return <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text' />
+        <label htmlFor='age'>Age(years)</label>
+        <input id='age' type='number' />
+        <button type='submit'>Add user</button>
+    </form>
+}
+
+export default AddUser;
